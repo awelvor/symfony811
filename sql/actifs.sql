d49379548b3adb760ccf08a554a946487503a31a
awelvor <philippe.regent@gmail.com>
2026-08-01 10:28:41 +0200

    Fix SQL syntax for selecting total from actifs

--- sql/actifs.sql
+++ sql/actifs.sql
@@ -1,2 +1,3 @@
-select created_at, (c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+ c11+c13+c14+c15+c16+c17+c18+c19) as total
+select created_at, (c1+c2+c3+c4+c5+c6+c7+c8+c9+c10+c11+c13+c14+c15+c16+c17+c18+c19) as total
 from actifs
+order by created_at desc
