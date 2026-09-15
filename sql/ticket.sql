960459f01e830ec44013f1f5c69fbc20ebffd209
awelvor <philippe.regent@gmail.com>
2026-09-15 14:40:16 +0200

    Create ticket.sql

---
+++ sql/ticket.sql
@@ -0,0 +1,10 @@
+/* tickets sql */
+
+
+insert into bitcoin
+(compte, date, libelle, credit, debit,budget)
+values
+('cb boursorama','2026-09-14','la poste mobile', 0.0,69.84,'septembre2026'),
+
+
+insert into bitcoin
