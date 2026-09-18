e15e20517ede639250a6567f92028e3898ccf6c5
awelvor <philippe.regent@gmail.com>
2026-09-18 09:19:43 +0200

    Update ticket.sql

--- sql/ticket.sql
+++ sql/ticket.sql
@@ -6,6 +6,6 @@ insert into bitcoin
 values
 ('cb boursorama','2026-09-14','lidl'       , 0.0,69.84,'septembre2026',2082747),
 ('cb boursorama','2026-09-15','intermarché', 0.0,48.28,'septembre2026',1781323),
-('cb boursorama','2026-09-15','lidl'       , 0.0,63.75,'septembre2026',2082747),
+('cb boursorama','2026-09-15','lidl'       , 0.0,63.75,'septembre2026',2082747);
 
 
