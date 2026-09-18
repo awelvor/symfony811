e4f46ba50636a2ca679ec5b94cb5b54b3d4ed367
awelvor <philippe.regent@gmail.com>
2026-09-18 18:08:36 +0200

    Add new insert statement for bitcoin transactions

--- sql/ticket.sql
+++ sql/ticket.sql
@@ -8,4 +8,10 @@ values
 ('cb boursorama','2026-09-15','intermarché', 0.0,48.28,'septembre2026',1781323),
 ('cb boursorama','2026-09-15','lidl'       , 0.0,63.75,'septembre2026',2082747);
 
+insert into bitcoin
+(compte, date, libelle, credit, debit,banque, budget,cbid)
+values
+('cb boursorama','2026-09-18','intermarché', 0.0,49.58,'2026-09-18','septembre2026',1372014),
+
+
 
