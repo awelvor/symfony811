01ad433f5cd7082e9cd6e2581d8a5bd12cbfc98f
awelvor <philippe.regent@gmail.com>
2026-09-21 10:55:33 +0200

    Add new transactions to ticket.sql

--- sql/ticket.sql
+++ sql/ticket.sql
@@ -11,9 +11,11 @@ values
 insert into bitcoin
 (compte, date, libelle, credit, debit,banque, budget,cbid)
 values
-('cb boursorama','2026-09-18','intermarché', 0.0,49.58,'2026-09-19','septembre2026',1372014),
-('cb boursorama','2026-09-19','super u'    , 0.0,21.23,'2026-09-19','septembre2026',9230893),
-('cb boursorama','2026-09-19','tea vanni'  , 0.0, 2.70,'2026-09-19','septembre2026',0407446),
+('cb boursorama','2026-09-18','intermarché'   , 0.0,49.58,'2026-09-19','septembre2026',1372014),
+('cb boursorama','2026-09-19','super u'       , 0.0,21.23,'2026-09-19','septembre2026',9230893),
+('cb boursorama','2026-09-19','tea vanni'     , 0.0, 2.70,'2026-09-19','septembre2026',0407446),
+('cc boursorama','2026-09-20','P. CULTE CATHO', 0.0, 5.00,'2026-09-20','septembre2026',NULL),
+('cb boursorama','2026-09-21','HPY VEEPEE'    , 0.0,97.99,'2026-09-21','septembre2026',NULL),
 
 
 
